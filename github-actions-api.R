15a9dc3523a46f4af817b99a9918676f937ed801
Beatriz Milz <milz.bea@gmail.com>
2022-06-04 21:06:12 -0300

    update actions

--- github-actions-api.R
+++ github-actions-api.R
@@ -46,6 +46,9 @@ rstudio_repositories <- rstudio_orgs |>
 original_rstudio_repos <- rstudio_repositories |>
   dplyr::filter(fork == FALSE)
 
+readr::write_rds(original_rstudio_repos,
+                 glue::glue("data/original_rstudio_repos_{Sys.Date()}.Rds"))
+
 get_repo_github_actions <- function(repo_full_name){
 
   workflows_list <- gh::gh("GET /repos/{full_name}/actions/workflows",
@@ -61,49 +64,29 @@ get_repo_github_actions <- function(repo_full_name){
 }
 
 
-rstudio_actions <- original_rstudio_repos$full_name |>
-  purrr::map_dfr(get_repo_github_actions)
+# rstudio_actions <- original_rstudio_repos$full_name |>
+#   purrr::map_dfr(get_repo_github_actions)
 
 
 
 
 # viz - creating of actions on repositories
-library(ggplot2)
-rstudio_actions |>
-  dplyr::filter(state == "active") |>
-  dplyr::mutate(created_at = lubridate::as_date(created_at),
-    created_month = lubridate::floor_date(created_at, "month")) |>
-  dplyr::count(created_month) |>
-  dplyr::mutate(n_acumulated = cumsum(n)) |>
-  ggplot() +
-  geom_line(aes(x = created_month, y = n_acumulated))
+# library(ggplot2)
+# rstudio_actions |>
+#   dplyr::filter(state == "active") |>
+#   dplyr::mutate(created_at = lubridate::as_date(created_at),
+#     created_month = lubridate::floor_date(created_at, "month")) |>
+#   dplyr::count(created_month) |>
+#   dplyr::mutate(n_acumulated = cumsum(n)) |>
+#   ggplot() +
+#   geom_line(aes(x = created_month, y = n_acumulated))
 
 
 
 
 
-get_repo_github_actions_runs_page <- function(repo_full_name, page_number){
 
-  runs_list <- gh::gh(
-    "GET /repos/{full_name}/actions/runs",
-    full_name = repo_full_name,
-    per_page = 100,
-    page = page_number
-  )
-
-  runs_df <- runs_list |>
-    purrr::pluck("workflow_runs") |>
-    purrr::map(purrr::compact) |>
-    purrr::map(purrr::discard, ~is.list(.x)) |>
-    purrr::map_dfr(tibble::as_tibble, .id = "id") |>
-    dplyr::mutate(repo = repo_full_name, .before = tidyselect::everything())
-
-
-  runs_df
-}
-
-
-get_repo_github_actions_runs_page <- function(repo_full_name) {
+get_repo_github_actions_runs <- function(repo_full_name) {
 
   total_count <- gh::gh(
     "GET /repos/{full_name}/actions/runs",
@@ -114,17 +97,30 @@ get_repo_github_actions_runs_page <- function(repo_full_name) {
 
   pages_to_iterate <- ceiling(total_count/100)
 
-  df_all_runs <- purrr::map_dfr(1:pages_to_iterate,
-                 ~ get_repo_github_actions_runs_page(
-    repo_full_name = repo_full_name,
-    page_number = .x
+  list_all_runs <- purrr::map_dfr(1:pages_to_iterate,
+                 ~ gh::gh(
+                   "GET /repos/{full_name}/actions/runs",
+                   full_name = repo_full_name,
+                   per_page = 100,
+                   page = .x
+                 ))
 
-  ))
+  runs_df <- list_all_runs |>
+    purrr::pluck("workflow_runs") |>
+    purrr::map(purrr::compact) |>
+    purrr::map(purrr::discard, ~is.list(.x)) |>
+    purrr::map_dfr(tibble::as_tibble, .id = "id") |>
+    dplyr::mutate(repo = repo_full_name, .before = tidyselect::everything())
 
-  df_all_runs
+
+  readr::write_rds(runs_df,
+                   glue::glue("data/repos/runs_{ stringr::str_replace(repo_full_name, '/', '_')}_{Sys.Date()}.Rds"))
+
+
 }
 
-
+original_rstudio_repos$full_name |>
+  purrr::walk(get_repo_github_actions_runs)
 
 
 df_all_runs |>
